ec006ecc623c486ae3cde276a93f5ba8f531d671
Rokzone <127233873+Curioso-Analyst@users.noreply.github.com>
2024-11-20 16:08:52 +0000

    Task 2
    Plots updated

--- LAB1_ROB.m
+++ LAB1_ROB.m
@@ -1,3 +1,5 @@
+%% Task 1
+
 % Read the data
 data = readmatrix('LAB1_9.TXT');
 
@@ -37,5 +39,63 @@ hold off;
 
 title('Angular Velocity Data vs Time');
 xlabel('Time (seconds)');
+ylabel('Angular Velocity Data (rad/s)');
+legend;
+
+%% Task 2
+
+% Apply median filter
+a_data_1_filtered = medfilt1(a_data_1, 5); % Janela de filtro = 5
+a_data_2_filtered = medfilt1(a_data_2, 5);
+a_data_3_filtered = medfilt1(a_data_3, 5);
+w_data_1_filtered = medfilt1(w_data_1, 5);
+w_data_2_filtered = medfilt1(w_data_2, 5);
+w_data_3_filtered = medfilt1(w_data_3, 5);
+
+% Plot original vs filtered acceleration data
+figure;
+subplot(2,1,1);
+plot(time, a_data_1, 'r', 'LineWidth', 2, 'DisplayName', 'Accelerometer X');
+hold on;
+plot(time, a_data_2, 'g', 'LineWidth', 2, 'DisplayName', 'Accelerometer Y');
+plot(time, a_data_3, 'b', 'LineWidth', 2, 'DisplayName', 'Accelerometer Z');
+hold off;
+title('Original Acceleration Data vs Time');
+xlabel('Time (s)');
+ylabel('Acceleration Data (m/s^{2})');
+legend;
+
+subplot(2,1,2);
+plot(time, a_data_1_filtered, 'r', 'LineWidth', 2, 'DisplayName', 'Accelerometer X');
+hold on;
+plot(time, a_data_2_filtered, 'g', 'LineWidth', 2, 'DisplayName', 'Accelerometer Y');
+plot(time, a_data_3_filtered, 'b', 'LineWidth', 2, 'DisplayName', 'Accelerometer Z');
+hold off;
+title('Filtered Acceleration Data vs Time');
+xlabel('Time (s)');
+ylabel('Acceleration Data (m/s^{2})');
+legend;
+
+% Plot original vs filtered angular velocity data
+figure;
+subplot(2,1,1);
+plot(time, w_data_1, 'r', 'LineWidth', 2, 'DisplayName', 'Angular velocity X');
+hold on;
+plot(time, w_data_2, 'g', 'LineWidth', 2, 'DisplayName', 'Angular velocity Y');
+plot(time, w_data_3, 'b', 'LineWidth', 2, 'DisplayName', 'Angular velocity Z');
+hold off;
+title('Original Angular Velocity Data vs Time');
+xlabel('Time (s)');
+ylabel('Angular Velocity Data (rad/s)');
+legend;
+
+subplot(2,1,2);
+plot(time, w_data_1_filtered, 'r', 'LineWidth', 2, 'DisplayName', 'Angular velocity X');
+hold on;
+plot(time, w_data_2_filtered, 'g', 'LineWidth', 2, 'DisplayName', 'Angular velocity Y');
+plot(time, w_data_3_filtered, 'b', 'LineWidth', 2, 'DisplayName', 'Angular velocity Z');
+hold off;
+title('Filtered Angular Velocity Data vs Time');
+xlabel('Time (s)');
 ylabel('Angular Velocity Data (rad/s)');
 legend;
