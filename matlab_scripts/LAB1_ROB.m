1a15e8c65593d216458f432b94db6e2cb6170259
JosePr400 <jpcr400@gmail.com>
2024-12-01 12:44:37 +0000

    Task 5 e 6 feito

--- matlab_scripts/LAB1_ROB.m
+++ matlab_scripts/LAB1_ROB.m
@@ -103,72 +103,198 @@ ylabel('Angular Velocity Data (rad/s)');
 legend;
 
 %% Task 5
-%Integration to obtain the orientation angles
-theta_x = cumtrapz(time, w_data_1_filtered); % Angle around the X-axis
-theta_y = cumtrapz(time, w_data_2_filtered); % Angle around the Y-axis
-theta_z = cumtrapz(time, w_data_3_filtered); % Angle around the Z-axis
+% Initialize variables for orientation
+old_theta_x = 0; % Initial angle on the X axis
+old_theta_y = 0; % Initial angle on the Y axis
+old_theta_z = 0; % Initial angle on the Z axis
 
-%Trajectory in the orientation space
+old_time = 0; % Initial time
+
+alpha = 0;
+beta = 0;
+lambda = 0;
+
+% Preallocate arrays to store results
+theta_x = zeros(size(time)); % Angle around the X axis
+theta_y = zeros(size(time)); % Angle around the Y axis
+theta_z = zeros(size(time)); % Angle around the Z axis
+
+% Loop to calculate orientations
+for i = 1:length(time)
+    % Time step
+    dt = time(i) - old_time;
+
+    alphak = cos(alpha) * (w_data_1_filtered(i) + sin(alpha) * tan(beta) * w_data_2_filtered(i) + cos(alpha) * tan(beta) * w_data_3_filtered(i));
+    betak = cos(alpha) * (cos(alpha) * w_data_2_filtered(i) + sin(alpha) * w_data_3_filtered(i));
+    lambdak = cos(alpha) * ((sin(alpha) / cos(beta)) * w_data_2_filtered(i) + (cos(alpha) / cos(beta)) * w_data_3_filtered(i));
+
+    % Update angles using motion equations
+    % theta = theta_old + w * dt
+    next_theta_x = old_theta_x + alphak * dt;
+    next_theta_y = old_theta_y + betak * dt;
+    next_theta_z = old_theta_z + lambdak * dt;
+
+    % Store results
+    theta_x(i) = next_theta_x;
+    theta_y(i) = next_theta_y;
+    theta_z(i) = next_theta_z;
+
+    % Update old variables for the next iteration
+    old_theta_x = next_theta_x;
+    old_theta_y = next_theta_y;
+    old_theta_z = next_theta_z;
+
+    old_time = time(i);
+end
+
+% Plot the 3D orientation trajectory
 figure;
-plot3(theta_x, theta_y, theta_z, 'LineWidth', 2);
-grid on;
+% Trajectory 1: Using motion equations
+plot3(theta_x, theta_y, theta_z, 'LineWidth', 1.5); % Orientation trajectory (Euler)
+
+% Adjust plot
+grid on; % Enable grid
 xlabel('\theta_x (rad)');
 ylabel('\theta_y (rad)');
 zlabel('\theta_z (rad)');
-title('Trajectory in Orientation Space');
-legend('Orientation Trajectory');
-
+title('3D Orientation Trajectory');
+legend('Trajectory (Motion Equations)');
+%view(3);
+%axis equal; % Ensure axes have the same scale
+%hold on; % Allow adding another plot to the same figure
 %% Task 6
-
 % Step 1: Integrate the acceleration to get velocity
 velocity_x = cumtrapz(time, a_data_1_filtered); % Velocity in the X direction
 velocity_y = cumtrapz(time, a_data_2_filtered); % Velocity in the Y direction
 velocity_z = cumtrapz(time, a_data_3_filtered); % Velocity in the Z direction
 
 % Step 2: Integrate the velocity to get position
-position_x = cumtrapz(time, velocity_x); % Position in the X direction
-position_y = cumtrapz(time, velocity_y); % Position in the Y direction
-position_z = cumtrapz(time, velocity_z); % Position in the Z direction
+position_x_cum = cumtrapz(time, velocity_x); % Position in the X direction
+position_y_cum = cumtrapz(time, velocity_y); % Position in the Y direction
+position_z_cum = cumtrapz(time, velocity_z); % Position in the Z direction
 
-% Step 3: Plot the 3D Cartesian trajectory
+% Initialize variables
+old_velocity_x = 0; % Initial velocity in the X axis
+old_velocity_y = 0; % Initial velocity in the Y axis
+old_velocity_z = 0; % Initial velocity in the Z axis
+
+old_pos_x = 0; % Initial position in the X axis
+old_pos_y = 0; % Initial position in the Y axis
+old_pos_z = 0; % Initial position in the Z axis
+
+old_time = 0; % Initial time
+
+% Preallocate arrays to store results
+velocity_x = zeros(size(time));
+velocity_y = zeros(size(time));
+velocity_z = zeros(size(time));
+
+position_x = zeros(size(time));
+position_y = zeros(size(time));
+position_z = zeros(size(time));
+
+% Loop to calculate velocities and positions
+for i = 1:length(time) % Start from the second point
+    % Time step
+    dt = time(i) - old_time;
+
+    % Update velocities using v = v_old + a * dt
+    next_velocity_x = old_velocity_x + a_data_1_filtered(i) * dt;
+    next_velocity_y = old_velocity_y + a_data_2_filtered(i) * dt;
+    next_velocity_z = old_velocity_z + a_data_3_filtered(i) * dt;
+
+    % Update positions using p = p_old + v_old * dt + 0.5 * a * dt^2
+    next_pos_x = old_pos_x + old_velocity_x * dt + 0.5 * a_data_1_filtered(i) * dt^2;
+    next_pos_y = old_pos_y + old_velocity_y * dt + 0.5 * a_data_2_filtered(i) * dt^2;
+    next_pos_z = old_pos_z + old_velocity_z * dt + 0.5 * a_data_3_filtered(i) * dt^2;
+
+    % Store results
+    velocity_x(i) = next_velocity_x;
+    velocity_y(i) = next_velocity_y;
+    velocity_z(i) = next_velocity_z;
+
+    position_x(i) = next_pos_x;
+    position_y(i) = next_pos_y;
+    position_z(i) = next_pos_z;
+
+    % Update old variables
+    old_velocity_x = next_velocity_x;
+    old_velocity_y = next_velocity_y;
+    old_velocity_z = next_velocity_z;
+
+    old_pos_x = next_pos_x;
+    old_pos_y = next_pos_y;
+    old_pos_z = next_pos_z;
+
+    old_time = time(i);
+end
+    
+% Plot the final 3D trajectory
+figure; % Create new figure
+plot3(position_x, position_y, position_z, 'LineWidth', 1.5); % Full trajectory
+hold on; % Allow adding another plot to the same figure
+plot3(position_x_cum, position_y_cum, position_z_cum, 'LineWidth', 2); % Orientation trajectory (cumtrapz)
+grid on; % Enable grid
+xlabel('Position X (m)'); % X-axis label
+ylabel('Position Y (m)'); % Y-axis label
+zlabel('Position Z (m)'); % Z-axis label
+title('Calculated 3D Trajectory'); % Plot title
+
+legend('Trajectory (Numerical)', 'Trajectory (cumtrapz)', 'Location', 'best'); 
+hold off;
+
+% Configure orientation arrow scale
+scale = 20; % Scale for arrow length
+
+% Create the figure
 figure;
-plot3(position_x, position_y, position_z, 'LineWidth', 2);
-grid on;
+hold on;
+
+% Position trajectory
+plot3(position_x, position_y, position_z, 'k', 'LineWidth', 1.5); % Trajectory in black
+scatter3(position_x, position_y, position_z, 10, 'filled'); % Points along the trajectory
+
+% Loop to draw orientations
+for i = 1:10:length(time) % Every 10 points
+    % Get current angles
+    tx = theta_x(i);
+    ty = theta_y(i);
+    tz = theta_z(i);
+
+    % Calculate rotation matrix manually
+    Rz = [cos(tz), -sin(tz), 0;
+          sin(tz),  cos(tz), 0;
+          0,        0,       1];
+    Ry = [cos(ty),  0, sin(ty);
+          0,        1, 0;
+         -sin(ty),  0, cos(ty)];
+    Rx = [1, 0,        0;
+          0, cos(tx), -sin(tx);
+          0, sin(tx),  cos(tx)];
+    
+    % Full rotation matrix (ZYX)
+    R = Rz * Ry * Rx;
+
+    % Locally oriented unit vectors (X, Y, Z local)
+    x_dir = R(:, 1) * scale; % Local X axis
+    y_dir = R(:, 2) * scale; % Local Y axis
+    z_dir = R(:, 3) * scale; % Local Z axis
+
+    % Current position
+    pos = [position_x(i), position_y(i), position_z(i)];
+
+    % Draw local axis vectors
+    quiver3(pos(1), pos(2), pos(3), x_dir(1), x_dir(2), x_dir(3), 'r', 'LineWidth', 1.5); % X axis in red
+    quiver3(pos(1), pos(2), pos(3), y_dir(1), y_dir(2), y_dir(3), 'g', 'LineWidth', 1.5); % Y axis in green
+    quiver3(pos(1), pos(2), pos(3), z_dir(1), z_dir(2), z_dir(3), 'b', 'LineWidth', 1.5); % Z axis in blue
+end
+
+% Adjust plot
 xlabel('Position X (m)');
 ylabel('Position Y (m)');
 zlabel('Position Z (m)');
-title('Reconstructed 3D Trajectory in Cartesian Space');
-legend('Trajectory in 3D Space');
-axis tight;
-
-
-% Animate 3D position trajectory
-figure;
-subplot(1,2,1);
-h1 = plot3(position_x(1), position_y(1), position_z(1), 'o'); % Initialize the plot with the first point
-axis([min(position_x) max(position_x) min(position_y) max(position_y) min(position_z) max(position_z)]);
-xlabel('Position X (m)');
-ylabel('Position Y (m)');
-zlabel('Position Z (m)');
-title('Animated 3D Position Trajectory');
+title('3D Trajectory with Local Orientations');
 grid on;
-
-for i = 2:length(position_x)
-    set(h1, 'XData', position_x(1:i), 'YData', position_y(1:i), 'ZData', position_z(1:i)); % Update the plot
-    pause(0.01); % Adjust the frame rate of the animation
-end
-
-% Animate orientation trajectory in 3D space
-subplot(1,2,2);
-h2 = plot3(theta_x(1), theta_y(1), theta_z(1), 'o'); % Initialize the plot with the first point
-axis([min(theta_x) max(theta_x) min(theta_y) max(theta_y) min(theta_z) max(theta_z)]);
-xlabel('\theta_x (rad)');
-ylabel('\theta_y (rad)');
-zlabel('\theta_z (rad)');
-title('Animated Orientation Trajectory');
-grid on;
-
-for i = 2:length(theta_x)
-    set(h2, 'XData', theta_x(1:i), 'YData', theta_y(1:i), 'ZData', theta_z(1:i)); % Update the plot
-    pause(0.01); % Adjust the frame rate of the animation
-end
+view(3); % 3D
+legend({'Trajectory', 'Position','Orientation X', 'Orientation Y', 'Orientation Z'}, 'Location', 'best');
+hold off;
